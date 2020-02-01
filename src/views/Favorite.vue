756c4da80fa1b94b26d1ab899ce9b110a35cf540
wanghuanle <wanghuanle@qq.com>
2020-02-01 17:38:37 +0800

    首页的完成

---
+++ src/views/Favorite.vue
@@ -0,0 +1,229 @@
+<template>
+	<div>
+		<div class="banner">
+			<svg class="star" viewBox="0 0 24 24" width="36" height="36">
+				<path d="M5.515 19.64l.918-5.355-3.89-3.792c-.926-.902-.639-1.784.64-1.97L8.56 7.74l2.404-4.871c.572-1.16 1.5-1.16 2.072 0L15.44 7.74l5.377.782c1.28.186 1.566 1.068.64 1.97l-3.89 3.793.918 5.354c.219 1.274-.532 1.82-1.676 1.218L12 18.33l-4.808 2.528c-1.145.602-1.896.056-1.677-1.218z"
+				 fill-rule="evenodd"></path>
+			</svg>
+			<h3 style="margin-left:10px;font-weight: bold;">热门收藏夹</h3>
+		</div>
+		<div class="container">
+			<div class="row" v-for="(item, index) in favorites" :key="index">
+				<div class="left">
+					<div class="top1">
+						<h4 class="title-set">{{ item.title }}</h4>
+					</div>
+					<div class="middle1">
+						<img :src="item.creatorAvatar" alt="" class="img-avatara" />
+						<h3 style="margin-left:60px;margin-top: -35px;font-size: 18px;">{{ item.creatorName }}&nbsp;&nbsp;创建</h3>
+					</div>
+					<div class="bottom">
+						<div class="w-row">
+							<div class="right1">
+								<button class="button2">
+									<h4 style="color: #1E90FF;font-size: 1.125rem;font-weight: bold;">关注收藏夹</h4>
+								</button>
+							</div>
+						</div>
+						<h4 class="follower">{{ item.followers }}人关注</h4>
+					</div>
+				</div>
+				<!--竖线的写法 -->
+				<div style="float:left;margin-top: 24px;width: 1px;height: 200px; background: darkgray;margin-right: 25px;"></div>
+				<div class="right">
+					<div class="top2">
+						<h4 class="title-set1">{{ item.questionTitle }}</h4>
+					</div>
+					<div class="middle2">
+						<h4 class="answer">{{ item.answerAuthorName }}</h4>
+					</div>
+					<div class="bottom2">
+						<button class="button-topic">回答</button>
+						<h4 class="tz">{{ item.voteupCount }}赞同·{{ item.commentCount }}评论</h4>
+					</div>
+					<div class="bottom3">
+						<h4 class="totalCount">已收藏{{ item.totalCount }}条内容></h4>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				favorites: []
+			};
+		},
+		created() {
+			this.axios({
+				method: 'get',
+				url: 'http://localhost:8080/api/favorite/all',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+			}).then(res => {
+				this.favorites = res.data.data;
+				console.log(res.data.data);
+			});
+		},
+	};
+</script>
+
+<style lang="scss" scoped>
+	.img-avatara {
+		height: 2.5rem;
+		width: 2.5rem;
+		margin-top: 50px;
+		margin-left: 0.625rem;
+	}
+
+	.follower {
+		margin-top: -30px;
+		margin-left: 120px;
+		font-size: 18px;
+	}
+
+	.tz {
+		font-size: 15px;
+		margin-top: -25px;
+		margin-left: 60px;
+	}
+
+	.answer {
+		font-size: 1.125rem;
+		color: #333333;
+		margin-top: 20px;
+	}
+
+	h3 {
+		color: #1a1a1a;
+		font-size: 30px;
+	}
+
+	.totalCount {
+		margin-top: 40px;
+		font-size: 18px;
+	}
+
+	h4 {
+		color: #a8b2b4;
+		font-size: 20px;
+	}
+
+	.title-set {
+		font-size: 1.125rem;
+		font-weight: bold;
+		color: black;
+		margin-top: 20px;
+	}
+
+	.title-set1 {
+		font-size: 1.125rem;
+		font-weight: bold;
+		color: black;
+		margin-top: 15px;
+	}
+
+	.img1 {
+		width: 30px;
+		height: 30px;
+		margin-top: 15px;
+	}
+
+	.button {
+		width: 6.25rem;
+		border: none;
+		background-color: rgb(235, 245, 255);
+		color: rgb(30, 134, 255);
+		height: 40px;
+		border-radius: 5px;
+		margin-left: 40.625rem;
+		margin-top: -9.375rem;
+	}
+
+	.left {
+		margin-left: 1.25rem;
+		width: 35%;
+	}
+
+	.right {
+		width: 65%;
+	}
+
+	.title-color {
+		color: black;
+		font-size: 1.25rem;
+		font-weight: bold;
+	}
+
+	.button-topic {
+		border: none;
+		width: 40px;
+		background-color: lightgray;
+		height: 30px;
+		font-size: 15px;
+		font-weight: bold;
+		margin-top: 15px;
+	}
+
+	.meta {
+		margin-top: -0.625rem;
+		color: gray;
+	}
+
+	.banner {
+		margin-bottom: 10px;
+		margin-top: 70px;
+		height: 100px;
+		border: 1px solid #d6d6d6;
+		box-shadow: 2px 5px 5px #ddd;
+		padding-left: 10;
+		display: flex;
+		align-items: center;
+
+		img {
+			width: 12.5rem;
+			height: 6.25rem;
+		}
+	}
+
+	.button2 {
+		width: 30%;
+		border: none;
+		background-color: rgb(235, 245, 255);
+		color: rgb(30, 134, 255);
+		height: 35px;
+		border-radius: 5px;
+		
+		margin-top: 60px;
+	}
+
+	.container {
+		overflow: auto;
+		margin: 0 auto;
+		width: 70%;
+
+		.row {
+			background-color: #ffffff;
+			display: flex;
+			border: 1px solid #d6d6d6;
+			border-radius: 4px;
+			height: auto;
+			padding: 14px;
+			box-shadow: 0 1px 3px 0 rgba(26, 26, 26, 0.1);
+		}
+	}
+
+	.introduction {
+		font-size: 18px;
+		// color: #5c5e5c;
+		margin-top: 0.625rem;
+	}
+
+	.star {
+		fill: #0084ff;
+	}
+</style>
---
+++ src/views/RoundTable.vue
@@ -0,0 +1,118 @@
+<template>
+	<div class="wrapper">
+		<div class="container">
+			<div class="top">
+				<p class="roundtable">圆桌</p>
+				<p class="link">举办圆桌</p>
+			</div>
+			<div class="row">
+				<div class="col-3" v-for="(item, index) in roundTables" :key="index">
+					<div class="mask"></div>
+					<img :src="item.banner" alt="" />
+					<p class="name">{{ item.name }}</p>
+					<p class="visits_count">该圆桌被浏览{{ item.visitsCount }}次</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'roundTables',
+		// data一定要是个函数
+		data() {
+			return {
+				roundTables: []
+			};
+		},
+		created() {
+			this.axios({
+				method: 'get',
+				url: 'http://localhost:8080/api/round/table/all',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+			}).then(res => {
+				this.roundTables = res.data.data;
+				console.log(res.data.data);
+			});
+		}
+	};
+</script>
+<style lang="scss" scoped>
+	.wrapper {
+		margin-top: 10px;
+		.container {
+			width: 80%;
+			margin: 0 auto;
+			padding: 5px;
+			background-color: #fff;
+			.top {
+				display: flex;
+				/*项目位于各行之间留有空白的容器内。*/
+				justify-content: space-between;
+				padding:15px 55px 15px 50px;
+				font-size: 15px;
+				font-weight: 600;
+				.roundtable{
+					color: #1a1a1a;
+				}
+				.link {
+					text-decoration: none;
+					color: #0c89ff;
+					cursor: pointer;
+				}
+				border-radius: 3px;
+				box-shadow: 0 1px 3px 0 rgba(0, 34, 77, 0.05);
+			}
+			.row {
+				text-align: center;
+				.col-3 {
+					display: inline-block;
+					position: relative;
+					width: 200px;
+					height: 230px;
+					margin: 15px;
+					border-radius: 5px;
+					padding-left: 5px;
+					.mask {
+						position: absolute;
+						top: 0;
+						left: 0;
+						right: 0;
+						bottom: 30px;
+						width: 200px;
+						height: 200px;
+						box-shadow: 0 2px 10px 0 rgba(0, 0, 0, 0.1);
+						background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
+						z-index: 5;
+					}
+					.name {
+						position: absolute;
+						font-size: 15px;
+						color: #fff;
+						bottom: 40px;
+						z-index: 10;
+					}
+					img {
+						position: absolute;
+						top: 0;
+						left: 0;
+						right: 0;
+						width: 200px;
+						height: 200px;
+						bottom: 30px;
+						border-radius: 5px;
+					}
+					.visits_count {
+						position: absolute;
+						bottom: 0;
+						font-size: 15px;
+						color: #8590A6;
+					}
+				}
+			}
+		}
+	}
+</style>
